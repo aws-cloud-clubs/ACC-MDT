af1024878ae36c9562fa3712e68271707c4b9741
Chaeyeon Park <chay322p@gmail.com>
2024-07-30 02:34:20 +0900

    [Feat] : AwsConfig S3 설정 - S3관련 설정 추가 - credentials 생성 로직 함수화

--- src/main/java/com/mdt/backend/config/AwsConfig.java
+++ src/main/java/com/mdt/backend/config/AwsConfig.java
@@ -5,42 +5,59 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AwsConfig {
 
-    @Value("${spring.cloud.aws.credentials.access-key}")
-    private String accessKey;
+  @Value("${spring.cloud.aws.credentials.access-key}")
+  private String accessKey;
 
-    @Value("${spring.cloud.aws.credentials.secret-key}")
-    private String secretKey;
+  @Value("${spring.cloud.aws.credentials.secret-key}")
+  private String secretKey;
 
-    @Value("${spring.cloud.aws.region.static}")
-    private String region;
+  @Value("${spring.cloud.aws.region.static}")
+  private String region;
 
-    @Bean
-    public AmazonDynamoDBClient dynamoDBClient() {
-        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
-        return (AmazonDynamoDBClient) AmazonDynamoDBClient.builder()
-            .withRegion(region)
-            .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
-            .build();
-    }
+  @Bean
+  @Primary
+  public BasicAWSCredentials awsCredentialsProvider() {
+    BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+    return basicAWSCredentials;
+  }
 
-    @Bean
-    public DynamoDBMapper dynamoDBMapper() {
-        DynamoDBMapperConfig builder = DynamoDBMapperConfig.builder().
-            withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.CLOBBER) // update 시 덮어쓰기
-            .withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT) // 강력한 일관성 적용
-            .withTableNameOverride(null) // 테이블 이름 재정의 x
-            .withPaginationLoadingStrategy(DynamoDBMapperConfig.PaginationLoadingStrategy.EAGER_LOADING) //  페이지네이션을 통해서 가져오기보다 모든 내용을 다 가져오는 방식
-            .build();
+  @Bean
+  public AmazonDynamoDBClient dynamoDBClient() {
+    return (AmazonDynamoDBClient) AmazonDynamoDBClient.builder()
+        .withRegion(region)
+        .withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
+        .build();
+  }
 
-        return new DynamoDBMapper(dynamoDBClient(), builder);
-    }
+  @Bean
+  public DynamoDBMapper dynamoDBMapper() {
+    DynamoDBMapperConfig builder = DynamoDBMapperConfig.builder().
+        withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.CLOBBER) // update 시 덮어쓰기
+        .withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT) // 강력한 일관성 적용
+        .withTableNameOverride(null) // 테이블 이름 재정의 x
+        .withPaginationLoadingStrategy(
+            DynamoDBMapperConfig.PaginationLoadingStrategy.EAGER_LOADING) //  페이지네이션을 통해서 가져오기보다 모든 내용을 다 가져오는 방식
+        .build();
 
+    return new DynamoDBMapper(dynamoDBClient(), builder);
+  }
 
+  @Bean
+  public AmazonS3 amazonS3() {
+    AmazonS3 s3Builder = AmazonS3ClientBuilder.standard()
+        .withRegion(region)
+        .withCredentials(new AWSStaticCredentialsProvider(awsCredentialsProvider()))
+        .build();
+    return s3Builder;
+  }
 }
