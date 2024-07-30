dd1f2b9275cd6b1c72d673689c48aa212b5f4479
cowboysj <su10jin11@khu.ac.kr>
2024-07-30 21:46:53 +0900

    [Feat] : AWSConfig S3 설정 추가

--- src/main/java/com/mdt/backend/config/AwsConfig.java
+++ src/main/java/com/mdt/backend/config/AwsConfig.java
@@ -5,6 +5,8 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,5 +44,12 @@ public class AwsConfig {
         return new DynamoDBMapper(dynamoDBClient(), builder);
     }
 
-
+    @Bean
+    public AmazonS3 amazonS3() {
+        BasicAWSCredentials basicAWSCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(basicAWSCredentials))
+                .build();
+    }
 }
