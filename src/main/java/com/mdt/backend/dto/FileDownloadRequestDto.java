911136057d0c45906f94b77a40897df9caad8fb2
cowboysj <su10jin11@khu.ac.kr>
2024-07-30 22:46:56 +0900

    [Feat] : 파일 다운로드 API 구현

---
+++ src/main/java/com/mdt/backend/dto/FileDownloadRequestDto.java
@@ -0,0 +1,11 @@
+package com.mdt.backend.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class FileDownloadRequestDto {
+    private String filePath;
+
+}
