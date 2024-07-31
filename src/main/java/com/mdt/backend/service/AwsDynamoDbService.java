d64288d201d0421735cf56d17ebe971818f2d0da
Khyojun <khyojun1104@gmail.com>
2024-07-31 22:30:28 +0900

    [Feat] : 임시 데이터 넣기 위한 주석 제거

--- src/main/java/com/mdt/backend/service/AwsDynamoDbService.java
+++ src/main/java/com/mdt/backend/service/AwsDynamoDbService.java
@@ -15,52 +15,6 @@ public class AwsDynamoDbService implements DbService {
 
     private final FileInfoRepository fileInfoRepository;
 
-    // 테스트를 위해 한 번 12000개의 임시 데이터를 넣었습니다.
-    //  1번 케이스 -> 유저 이름 파일 이름 둘 다 다른 경우
-    // 2번 케이스 -> 유저 이름 다르지만 파일 이름이 동일한 경우
-    /*
-    @PostConstruct // 테스트를 위한 데이터 2개를 넣었습니다! -> 계속 덮어쓰기로 진행
-    public void putTestDate(){
-
-        for(int i=0; i<6000; i++){  // 1번
-            String userId="user"+i;
-            String fileName=i+".txt";
-            int fileSize=1024;
-            String fileType="txt";
-            int fileContentLength=100;
-            String createdAt="2024.07.28";
-            fileInfoRepository.save(FileInfo.builder().
-                userId(userId).
-                fileName(fileName).
-                fileSize(fileSize).
-                fileType(fileType).
-                fileContentLength(fileContentLength).
-                createdAt(createdAt).
-                build());
-        }
-
-
-        for(int i=0; i<6000; i++){ // 2번
-            String userId="user"+i;
-            String fileName="index.html";
-            int fileSize=1024;
-            String fileType="txt";
-            int fileContentLength=100;
-            String createdAt="2024.07.28";
-            fileInfoRepository.save(FileInfo.builder().
-                userId(userId).
-                fileName(fileName).
-                fileSize(fileSize).
-                fileType(fileType).
-                fileContentLength(fileContentLength).
-                createdAt(createdAt).
-                build());
-        }
-    }
-    */
-
-
-
     @Override
     public List<String> searchFiles(FileSearchRequestDto dto) {
         if (isRequestFilePathEmpty(dto))
