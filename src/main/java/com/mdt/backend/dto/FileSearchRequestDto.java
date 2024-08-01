c5f1bbc4dec844217532f26982aad3e84d189f18
Khyojun <khyojun1104@gmail.com>
2024-08-01 23:27:43 +0900

    [Fix] : reqeustBody 방식을 requestParam으로 수정하는 작업 진행

--- src/main/java/com/mdt/backend/dto/FileSearchRequestDto.java
+++ src/main/java/com/mdt/backend/dto/FileSearchRequestDto.java
@@ -2,8 +2,10 @@ package com.mdt.backend.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class FileSearchRequestDto {
     private String fileName;
